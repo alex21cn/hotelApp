c6fc6a6bc8c2446ec531f8544ac315f989ca0ecc
Alex Wang <alex.wxy@hotmail.com>
2016-10-25 17:53:54 -0400

    Added material 2

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -4,7 +4,8 @@ import { Component, OnInit } from "@angular/core";
     selector: "hotel-app",
     moduleId: module.id,
     //template: require("templates/app.template.html"),
-    templateUrl: "../templates/app.template.html"
+    templateUrl: "../templates/app.template.html",
+    styleUrls: ["../../asset/css/app.theme.css","../../asset/css/app.component.css"]
 })
 
 
